770a6b7215d341cbe5fedf040289834a17e7a101
alicezehner <alicezehner88@gmail.com>
2022-04-13 10:16:13 -0400

    save

--- zip.py
+++ zip.py
@@ -1,1 +1,18 @@
-#!/alicezehner/1.00/python
+import pgeocode
+import numpy
+import pandas
+
+input = 19035
+# country is always us
+country = pgeocode.Nominatim('us')
+
+# input zipcode --> make dynamic
+zipcode = country.query_postal_code(input)
+
+lat = zipcode["latitude"]
+lon = zipcode["longitude"]
+
+print(lat)
+print(lon)
+
+FLASK?
