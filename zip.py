b4627126487ea49c285e36fc6c1ac4ef61c3dafe
alicezehner <alicezehner88@gmail.com>
2022-04-14 19:49:22 -0400

    update

--- zip.py
+++ zip.py
@@ -1,18 +1,25 @@
 import pgeocode
 import numpy
 import pandas
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-input = 19035
-# country is always us
-country = pgeocode.Nominatim('us')
+@app.route('/')
+def index():
+  return render_template('index.html')
 
-# input zipcode --> make dynamic
-zipcode = country.query_postal_code(input)
+@app.route('/info', methods='POST')
+def zipcode():
+    if request.method == 'POST':
+        input = request.form['zipcode']
+        # country is always us
+        country = pgeocode.Nominatim('us')
+        
+        # input zipcode --> make dynamic
+        zipcode = country.query_postal_code(input)
 
-lat = zipcode["latitude"]
-lon = zipcode["longitude"]
-
-print(lat)
-print(lon)
-
-FLASK?
+        lat = zipcode["latitude"]
+        lon = zipcode["longitude"]
+        return render_template('info.html', lat=lat, lon=lon)
+    else:
+        return render_template('index.html') 
