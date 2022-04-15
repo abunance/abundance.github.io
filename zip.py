67273f62d4b94257d34f5786035dfc77df9071db
alicezehner <alicezehner88@gmail.com>
2022-04-14 20:40:24 -0400

    d

--- zip.py
+++ zip.py
@@ -4,21 +4,20 @@ import pandas
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=['GET'])
 def index():
-    if request.method=='GET':
-        return render_template("index.html")
-    if request.method == 'POST':
-        data = []
-        input = request.form['zipcode']
-        # country is always us
-        country = pgeocode.Nominatim('us')
+    return render_template("index.html")
         
-        # input zipcode --> make dynamic
-        zipcode = country.query_postal_code(input)
-        data.append(zipcode["latitude"])
-        data.append(zipcode["longitude"])
-        print(data)
-        #return render_template("info.html", data=data)
-
-#@app.route('/info', methods=['POST', 'GET'])
+@app.route('/info', methods=['POST'])
+def info():
+    data = []
+    input = request.form['zipcode']
+    # country is always us
+    country = pgeocode.Nominatim('us')
+        
+    # input zipcode --> make dynamic
+    zipcode = country.query_postal_code(input)
+    data.append(zipcode["latitude"])
+    data.append(zipcode["longitude"])
+    print(data)
+    return render_template("info.html", data=data)
