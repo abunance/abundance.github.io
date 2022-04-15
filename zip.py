886d18f2f526e2cf40b24997f05fac32b157b078
alicezehner <alicezehner88@gmail.com>
2022-04-14 21:26:01 -0400

    a

--- zip.py
+++ zip.py
@@ -1,27 +1,27 @@
 import pgeocode
 import numpy
 import pandas
+import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    error = None
     if request.method == "GET":
         return render_template("index.html")
     else: 
-        gh = request.form.get['zipcode']
-        return render_template("info.html", gh=gh)
-
-#@app.route('/info', methods=['POST'])
-#def info():
-    #data = []
-    #input = request.form.get['zipcode']
-    # country is always us
-    #country = pgeocode.Nominatim('us')
-        
-    # input zipcode --> make dynamic
-    #zipcode = country.query_postal_code(input)
-    #data.append(zipcode["latitude"])
-    #data.append(zipcode["longitude"])
-    
-    #pattern="\d{5}"
+        pattern= re.compile("\d{5}")
+        input = request.form['zipcode']
+        if pattern.match(input):
+            data = []
+            # country is always us
+            country = pgeocode.Nominatim('us')
+             
+            zipcode = country.query_postal_code(input)
+            data.append(zipcode["latitude"])
+            data.append(zipcode["longitude"])
+            return render_template("info.html", data=data)
+        else:
+            error = "Invalid Zipcode"
+            return render_template("index.html", error=error)
