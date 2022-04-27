4823a493a231c6cfecb8613044eaf55118a6a9f6
alicezehner <alicezehner88@gmail.com>
2022-04-27 19:48:54 -0400

    d

--- testing.py
+++ testing.py
@@ -1,13 +1,38 @@
-zipcode = '00601'
+input = '00601'
 
 import csv
+import pandas
 
+# read in weather data as a dict
 with open('weather.csv', mode='r') as infile:
     reader = csv.reader(infile)
     weatherdata = {rows[0]:rows[3] for rows in reader}
+
+# extract zipcode keys and precp values 
 keys = list(weatherdata.keys())
 values = list(weatherdata.values())
 
-index = keys.index(zipcode)
-x = values[index]
+# match inputed zipcode to precp value by indexing both lists
+index = keys.index(input)
+
 # FEED SPECIFIC TEMPERATURE AND WATER
+x = values[index]
+y = 15
+#must convert x string to float for calculations
+z = float(x)
+
+# converting csv to html
+croplist = ['CROP','WATERmm','TEMPC','WATER','TEMP']
+croptable = pandas.read_csv('crops.csv', usecols=croplist)
+weighted=[]
+
+# extract temp and water for each crop
+for index, row in croptable.iterrows():
+    T = row['TEMPC']
+    W = row['WATERmm']
+    # created weighted similarity to region's temp and water
+    weighted.append(abs(z-W)*.4/z+abs(y-T)*.6/y)
+croptable['similarity'] = weighted
+
+# sort crops by similarity
+cropsort = croptable.sort_values(by=['similarity'])
--- zip.py
+++ zip.py
@@ -12,31 +12,42 @@ def index():
         return render_template("index.html")
     else: 
         pattern= re.compile("\d{5}")
-        input = request.form['zipcode']
+        input = str(request.form['zipcode'])
         season = request.form.get('season')
         
         if pattern.match(input) and season in ['winter', 'spring', 'summer', 'autumn']: 
+            # read in weather data as a dict
             with open('weather.csv', mode='r') as infile:
                 reader = csv.reader(infile)
                 weatherdata = {rows[0]:rows[3] for rows in reader}
+
+            # extract zipcode keys and precp values 
             keys = list(weatherdata.keys())
             values = list(weatherdata.values())
 
+            # match inputed zipcode to precp value by indexing both lists
             index = keys.index(input)
-            x = values[index]
+
             # FEED SPECIFIC TEMPERATURE AND WATER
+            x = values[index]
             y = 15
+            #must convert x string to float for calculations
+            z = float(x)
 
             # converting csv to html
             croplist = ['CROP','WATERmm','TEMPC','WATER','TEMP']
             croptable = pandas.read_csv('crops.csv', usecols=croplist)
             weighted=[]
+
+            # extract temp and water for each crop
             for index, row in croptable.iterrows():
                 T = row['TEMPC']
                 W = row['WATERmm']
-                weighted.append(abs(x-W)*.4/x+abs(y-T)*.6/y)
-
+                # created weighted similarity to region's temp and water
+                weighted.append(abs(z-W)*.4/z+abs(y-T)*.6/y)
             croptable['similarity'] = weighted
+
+            # sort crops by similarity
             cropsort = croptable.sort_values(by=['similarity'])
     
             return render_template("info.html", input=input, season=season, crops=[cropsort.values.tolist()], titles=[''], prep=x, temp=y)
