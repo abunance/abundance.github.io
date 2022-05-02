4a63fc6431f7f1a3a4334d43c28d0831cf626e62
alicezehner <alicezehner88@gmail.com>
2022-05-02 10:32:53 -0400

    route

--- zip.py
+++ zip.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
 @app.route("/", methods=["POST", "GET"])
 def index():
